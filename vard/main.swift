242ca5877c0d3ea6b2c78468eca8ae9a6ef9e3eb
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2020-02-16 19:49:57 +0100

    Initial

---
+++ vard/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  vard
+//
+//  Created by Vitalii Kuznetsov on 16/02/2020.
+//  Copyright © 2020 Vitalii Kuznetsov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
